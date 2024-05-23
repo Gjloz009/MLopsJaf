d1d39cf1e3704281b74ebef4826918529edcd485
Gustavo Jafet <61368498+Gjloz009@users.noreply.github.com>
2024-05-23 22:26:01 +0000

    created model file

--- homework.py
+++ homework.py
@@ -1,3 +1,104 @@
 import pandas as pd 
-import sklearn as 
-2+2
+import pickle
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import Ridge
+
+from sklearn.metrics import mean_squared_error
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_parquet('/workspaces/MLopsJaf/data/yellow_tripdata_2024-01.parquet')
+
+df.columns
+df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
+df.duration = df.duration.apply(lambda td: td.total_seconds() / 60)
+
+df = df[(df.duration >= 1) & (df.duration <= 60)]
+
+categorical = ['PULocationID', 'DOLocationID']
+numerical = ['trip_distance']
+
+df[categorical] = df[categorical].astype(str)
+
+train_dicts = df[categorical + numerical].to_dict(orient='records')
+
+dv = DictVectorizer()
+X_train = dv.fit_transform(train_dicts)
+
+target = 'duration'
+y_train = df[target].values
+
+lr = LinearRegression()
+lr.fit(X_train, y_train)
+
+y_pred = lr.predict(X_train)
+
+mean_squared_error(y_train, y_pred, squared=False)
+
+sns.displot(y_pred, label='prediction')
+sns.displot(y_train, label='actual')
+
+plt.legend()
+plt.show()
+
+def read_dataframe(filename):
+    if filename.endswith('.csv'):
+        df = pd.read_csv(filename)
+
+        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    elif filename.endswith('.parquet'):
+        df = pd.read_parquet(filename)
+
+    df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
+    df.duration = df.duration.apply(lambda td: td.total_seconds() / 60)
+
+    df = df[(df.duration >= 1) & (df.duration <= 60)]
+
+    categorical = ['PULocationID', 'DOLocationID']
+    df[categorical] = df[categorical].astype(str)
+    
+    return df
+
+df_train = read_dataframe('/workspaces/MLopsJaf/data/yellow_tripdata_2024-01.parquet')
+df_val = read_dataframe('/workspaces/MLopsJaf/data/yellow_tripdata_2024-02.parquet')
+
+len(df_train), len(df_val)
+
+df_train['PU_DO'] = df_train['PULocationID'] + '_' + df_train['DOLocationID']
+df_val['PU_DO'] = df_val['PULocationID'] + '_' + df_val['DOLocationID']
+
+categorical = ['PU_DO'] #'PULocationID', 'DOLocationID']
+numerical = ['trip_distance']
+
+dv = DictVectorizer()
+
+train_dicts = df_train[categorical + numerical].to_dict(orient='records')
+X_train = dv.fit_transform(train_dicts)
+
+val_dicts = df_val[categorical + numerical].to_dict(orient='records')
+X_val = dv.transform(val_dicts)
+
+target = 'duration'
+y_train = df_train[target].values
+y_val = df_val[target].values
+
+lr = LinearRegression()
+lr.fit(X_train, y_train)
+
+y_pred = lr.predict(X_val)
+
+mean_squared_error(y_val, y_pred, squared=False)
+
+with open('models/lin_reg.bin', 'wb') as f_out:
+    pickle.dump((dv, lr), f_out)
+
+lr = Lasso(0.01)
+lr.fit(X_train, y_train)
+
+y_pred = lr.predict(X_val)
+
+mean_squared_error(y_val, y_pred, squared=False)
